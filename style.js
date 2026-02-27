1e6a659bf477f7b821588bdeb2280d494cfefda3
youhoun <youhounx@gmail.com>
2026-02-27 12:42:27 +0700

    Implement image slider with automatic slide transitions
    Added image slider functionality with automatic transitions.

--- style.js
+++ style.js
@@ -3,4 +3,20 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     document.getElementById("loader").style.display = "none";
   }, 1200);
+
 });
+const images = ["./photo1.jpg","./photo3.jpg","./photo5.jpg","./photo6.jpg"];
+let currentSlide = 0;
+const slider = document.getElementById("slider");
+
+function nextSlide(){
+    slider.style.opacity = 0;
+
+    setTimeout(()=>{
+        currentSlide = (currentSlide + 1) % images.length;
+        slider.src = images[currentSlide];
+        slider.style.opacity = 1;
+    }, 500);
+}
+
+setInterval(nextSlide, 4000);
